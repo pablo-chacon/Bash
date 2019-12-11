e0860d01880d1fa01b2c081c8ddfed5a52a4a0bc
Emil <58485004+ram1337@users.noreply.github.com>
2019-12-11 10:52:08 +0100

    Update sort_table.sh

--- sort_table.sh
+++ sort_table.sh
@@ -5,7 +5,7 @@
 # UTF-8 encode, Self sustaining script.  
 # 
 # Author: ekarlsson66@gmail.com
-# Version: 3.2
+# Version: 3.3
 # Date: 2019-12-03
 
 # In MAC addresses format: "000b-abe3-a2df"
@@ -20,20 +20,14 @@
 function read_table()
 {
 while read F
-do 
-     
+do     
     echo "$F" | grep ^"\d"  
-
 done
 }
 
 
-cat $1 | sed 's![-]!!g;s!:$!!' | sort -u -k2 \
-| awk '{ print $2" ; "$1 }' > "/tmp/mac.txt"
- 
-cat /tmp/mac.txt | iconv -f ISO-8859-1 -t UTF-8  &> "mac.csv" 
-
-rm -rf "/tmp/mac.txt"
+cat $1 | sed 's![-]!!g;s!:$!!' | sort -u -k2 | awk '{ print $2" ; "$1 }' \
+| iconv -f ISO-8859-1 -t UTF-8 &> "mac.csv"
 
 
 exit 0;   
